88e6f48885a0f13db881d3029bbdbbbeff54dec5
LEcombi <Leon.GitHub@outlook.de>
2025-04-11 23:10:24 +0200

    Update ping_host.py

--- functions/ping_host.py
+++ functions/ping_host.py
@@ -6,6 +6,14 @@ def ping_host():
     clear_screen.clear_screen()
     print("=== Ping Tool ===\n")
     host = input("Enter an IP address or domain: ")
+    times_ping = str(input("Enter the number of times to ping (default is 4)(type 0 to ping infinity): "))
     print("\nPinging...\n")
-    os.system(f"ping -c 4 {host}" if os.name != 'nt' else f"ping {host}")
+
+    if times_ping.strip() == "":
+        os.system(f"ping -c 4 {host}" if os.name != 'nt' else f"ping {host}")
+    elif times_ping == "0":
+        os.system(f"ping {host}" if os.name != 'nt' else f"ping {host}")
+    else:
+        os.system(f"ping -c {times_ping} {host}" if os.name != 'nt' else f"ping {host}")
+
     pause.pause() 
