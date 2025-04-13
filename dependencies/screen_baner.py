8104d4f20338ac0a04bf76483b964a6d0b3aca73
Leon <Leon.GitHub@outlook.de>
2025-04-13 08:54:33 +0000

    Fix incorrect function call in reload function in screen_baner.py

--- dependencies/screen_baner.py
+++ dependencies/screen_baner.py
@@ -2,5 +2,5 @@ import dependencies.banner as banner
 import dependencies.clear_screen as clear_screen
 
 def reload():
-    clear_screen.clear_screen.clear_screen()
+    clear_screen.clear_screen()
     banner.display_banner()
