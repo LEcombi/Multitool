c9e93e4873bd95a2a2d1c495dff15a960edadd66
Leon <Leon.GitHub@outlook.de>
2025-04-13 08:50:06 +0000

    Fix import statements in screen_baner.py for clarity and correct usage

--- dependencies/screen_baner.py
+++ dependencies/screen_baner.py
@@ -1,6 +1,6 @@
-import banner
-import clear_screen
+import dependencies.banner as banner
+import dependencies.clear_screen as clear_screen
 
 def reload():
-    clear_screen.clear_screen()
+    clear_screen.clear_screen.clear_screen()
     banner.display_banner()
